c14e20a397493e70873d4cbf3a50d89af32720ec
Brittany <brittany.m.richards3@gmail.com>
2024-02-02 01:47:05 +0000

    rearranged app for backend flask container to run

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -29,7 +29,7 @@ from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 # X-RAY --------------
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+
 
 # HoneyComb -----------
 # Initialize tracing and an exporter that can send data to Honeycomb
@@ -40,6 +40,7 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+XRayMiddleware(app, xray_recorder)
 
 # HoneyComb -----------
 # Initialize automatic instrumentation with Flask
